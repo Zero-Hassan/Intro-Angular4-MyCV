93a1d42a3551d5157b7e0861779e86f0805bd6c5
Zero-Hassan <mcdhass@gmail.com>
2017-02-10 21:36:39 +0100

    adding background image and lop efect

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,1 @@
-"use strict";console.log("'Allo 'Allo!");
+"use strict";var view=document.getElementById("view");document.addEventListener("mousemove",function(e){e.preventDefault();var t={x:e.clientX,y:e.clientY,b:e.clientX+230,a:e.clientY+150,d:e.clientX-230,c:e.clientY+150},c=("rect("+t.a+"px, "+t.b+"px, "+t.c+"px, "+t.d+"px)","circle(16.61% at "+t.x+"px "+t.y+"px)");console.log(c),view.style.display="block",view.style.clipPath=c},!0);
